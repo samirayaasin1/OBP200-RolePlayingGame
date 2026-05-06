01bcb0bee405078c40725bf021b4d0efdfa6f6d1
Samira <1hv.samirayasin@gmail.com>
2026-05-06 11:36:34 +0200

    refactor: move potion logic to potion class

--- OBP200-RolePlayingGame/Potions.cs
+++ OBP200-RolePlayingGame/Potions.cs
@@ -2,10 +2,17 @@ namespace OBP200_RolePlayingGame
 {
     public class Potion : IItem
     {
+        private const int HealAmount = 12;
+
         public void Use(Player player)
         {
-            player.UsePotion();
+            int oldHp = player.Hp;
+            player.Hp = Math.Min(player.MaxHp, player.Hp + HealAmount);
+
+            Console.WriteLine($"Du dricker en dryck och återfår {player.Hp - oldHp} HP.");
         }
+
+
     }
 }
 
